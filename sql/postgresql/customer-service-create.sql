01f9625413b42587e058b149dac3cd46d1a7c087
Benjamin Brink <kappa@dekka.com>
2013-01-10 09:33:27 -0800

    more data model blending

--- sql/postgresql/customer-service-create.sql
+++ sql/postgresql/customer-service-create.sql
@@ -2,11 +2,17 @@
 --
 -- @license GNU GENERAL PUBLIC LICENSE, Version 3
 
- 
+ -- instead of support_request.sequence_value which tracks sequential number
+ -- of tickets opened per day,
+ -- List number of tickets opened within a period TBD by parameter
+ -- or by number of open tickets ahead (by parameter)
+ -- or by average response time to opened tickets within (parameter period) and
+ --business hours (parameter). 
 create table cs_tickets (
     ticket_id           integer not null primary key,
     package_id          integer not null,
     customer_id         varchar(100),
+ -- authenticated_by is handy for indirect posts (such as via call center operator)
     authenticated_by    varchar(40),
     ticket_category_id  varchar(100),
     subject             varchar(100),
@@ -17,19 +23,28 @@ create table cs_tickets (
     -- a number, 0 no privacy requirements
     --           5 requires ssl to see, no content via notifications
     --           9 don't show to anyone but submitter via ssl
-    trashed_p            varchar(1)
+    trashed_p            varchar(1),
+    priority             integer,
+   -- for delegating/screening/escalating
+   -- app note. When these change, log to internal_notes in a new cs_ticket_message
+    assigned_to          integer,
+    assigned_by          integer
 );
 
 
 create table cs_ticket_messages (
+  -- aka support_content.id
     post_id          integer not null primary key,
     package_id       integer not null,
+  -- aka support_content.support_request_id
     ticket_id        integer not null,
+    posted_by        integer not null,
     post_time        timestamptz no null,
+  -- aka support_content.details
     message          text,
     internal_notes   text
     -- internal notes includes the values of variable_names_avail, if any;
-    -- and actions, such as cgi vars passed in a form, as well
+    -- and actions, such as ticket status changes, cgi vars passed in a form, as well
     -- as internal notes for ticket solvers etc.
 );
         
@@ -47,5 +62,13 @@ create table cs_message_templates (
     message_content  text,
     last_modified    timestamptz not null,
     last_modified_by integer,
+);
+
+CREATE TABLE cs_categories (
+    id integer not null,
+    label varchar(80),
+    grouping varchar(20),
+    active_p varchar(1) default '1',
+    description text
 );
 
