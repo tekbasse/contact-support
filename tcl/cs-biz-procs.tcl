d69c2210001eb4551a04cce765b7569d98a70e19
Benjamin Brink <tekbasse@yahoo.com>
2017-02-03 18:03:44 -0500

    moved some code, added some

--- tcl/cs-biz-procs.tcl
+++ tcl/cs-biz-procs.tcl
@@ -11,7 +11,27 @@ ad_library {
     
 }
 
-# cs_ticket_create (init new ticket, open, cs_ticket_message_create)
+ad_proc -public cs_ticket_create {
+    args
+} {
+    Create a ticket.
+} {
+    upvar 1 instance_id instance_id
+    # cs_ticket_create (init new ticket, open, cs_ticket_message_create)
+
+
+}
+
+ad_proc -private cs_ticket_message_create {
+    args
+} {
+    Create a message for a ticket_id
+} {
+    upvar 1 instance_id instance_id
+
+}
+
+
 # cs_announce (cs rep to multple customers) 
     # Send announcement / notifiy to subset of customers by customer_ref, ticket_id is the one
     # that is related to announcement. When ticket_id closes, announcement closes.
@@ -41,3 +61,63 @@ ad_library {
 # alert customers according to parameter SchedRemindersList
 #
 
+
+ad_proc -private cs_notify_customer_reps {
+    ticket_id
+    {subject ""}
+    {message ""}
+} {
+    Notify customer reps that subscribe to ticket.
+} {
+    ##code
+
+    # based on hf_monitor_alert_trigger (code extracted below)
+    # sender email is systemowner
+    # to get user_id of systemowner:
+    # party::get_by_email -email $email
+    set sysowner_email [ad_system_owner]
+    set sysowner_user_id [party::get_by_email -email $sysowner_email]
+
+    # What users to send alert to?
+    set users_list [cs_customer_reps_of_ticket $ticket_id]
+    if { [llength $users_list] > 0 } {
+        # get TO emails from user_id
+        set email_addrs_list [list ]
+        foreach uid $users_list {
+            lappend email_addrs_list [party::email -party_id $uid]
+        }
+        
+        # What else is needed to send alert message?
+        set ticket_ref [cs_t_ref_from_id $ticket_id]
+        set subject "#customer-service.ticket# #customer-service.Asset_Monitor# id ${monitor_id} for ${label}: ${alert_title}"
+        set body $alert_message
+        # post to logged in user pages 
+        hf_log_create $asset_id "#customer-service.Asset_Monitor#" "alert" "id ${monitor_id} ${subject} \n Message: ${body}" $user_id $instance_id 
+
+        # send email message
+        append body "#customer-service.Alerts_can_be_customized#. #customer-service.See_asset_configuration_for_details#."
+        acs_mail_lite::send -send_immediately $immediate_p -to_addr $email_addrs_list -from_addr $sysowner_email -subject $subject -body $body
+
+        # log/update alert status
+        if { $immediate_p } {
+            # show email has been sent
+            
+        } else {
+            # show email has been scheduled for sending.
+
+
+        }
+    }
+    return 1
+}
+
+
+ad_proc -private cs_notify_support_reps {
+    ticket
+} {
+    Notify support reps that subscribe to ticket.
+} {
+    ##code
+    # based on cs_notify_customer_reps
+
+}
--- tcl/cs-util-procs.tcl
+++ tcl/cs-util-procs.tcl
@@ -235,66 +235,6 @@ ad_proc -private cs_customer_reps_of_ticket {
 }
     
 
-ad_proc -private cs_notify_customer_reps {
-    ticket_id
-    {subject ""}
-    {message ""}
-} {
-    Notify customer reps that subscribe to ticket.
-} {
-    ##code
-
-    # based on hf_monitor_alert_trigger (code extracted below)
-    # sender email is systemowner
-    # to get user_id of systemowner:
-    # party::get_by_email -email $email
-    set sysowner_email [ad_system_owner]
-    set sysowner_user_id [party::get_by_email -email $sysowner_email]
-
-    # What users to send alert to?
-    set users_list [cs_customer_reps_of_ticket $ticket_id]
-    if { [llength $users_list] > 0 } {
-        # get TO emails from user_id
-        set email_addrs_list [list ]
-        foreach uid $users_list {
-            lappend email_addrs_list [party::email -party_id $uid]
-        }
-        
-        # What else is needed to send alert message?
-        set ticket_ref [cs_t_ref_from_id $ticket_id]
-        set subject "#customer-service.ticket# #customer-service.Asset_Monitor# id ${monitor_id} for ${label}: ${alert_title}"
-        set body $alert_message
-        # post to logged in user pages 
-        hf_log_create $asset_id "#customer-service.Asset_Monitor#" "alert" "id ${monitor_id} ${subject} \n Message: ${body}" $user_id $instance_id 
-
-        # send email message
-        append body "#customer-service.Alerts_can_be_customized#. #customer-service.See_asset_configuration_for_details#."
-        acs_mail_lite::send -send_immediately $immediate_p -to_addr $email_addrs_list -from_addr $sysowner_email -subject $subject -body $body
-
-        # log/update alert status
-        if { $immediate_p } {
-            # show email has been sent
-            
-        } else {
-            # show email has been scheduled for sending.
-
-
-        }
-    }
-    return 1
-}
-
-
-ad_proc -private cs_notify_support_reps {
-    ticket
-} {
-    Notify support reps that subscribe to ticket.
-} {
-    ##code
-    # based on cs_notify_customer_reps
-
-}
-
 
 
 ad_proc -private cs_ticket_action_log_cr {
