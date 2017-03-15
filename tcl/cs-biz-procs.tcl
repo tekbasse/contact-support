298334f2f2e6a7afae53e7b709a7bcebe6b84fc3
Benjamin Brink <tekbasse@yahoo.com>
2017-03-15 00:35:06 -0400

    more code

--- tcl/cs-biz-procs.tcl
+++ tcl/cs-biz-procs.tcl
@@ -74,44 +74,56 @@ ad_proc -public cs_ticket_create {
     set cs_time_opened [dt_systime]
     set user_open_p 1
     set user_time_opened $cs_time_opened
-    
-    set ticket_id [cs_id_seq_nextval ticket_ref]
-    #set ticket_ref  --corresponds to ticket_id
-    ns_log Notice "cs_ticket_create ticket_id '${ticket_id}' by user_id '${user_id}'"
-
-    if { $ann_type ne "" && [hf_list_filter_by_alphanum [list $ann_type]] } {
-        set ann_type [string range $ann_type 0 7]
-    } else {
-        set ann_type ""
-        ns_log Notice "cs_ticket_create: ann_type '${ann_type}' not valid. ignoring."
-    }
-
-    db_dml cs_tickets_cr {insert into cs_tickets
-        (ticket_id,instance_id,contact_id,authenticated_by,ticket_category_id,
-         current_tier_level,subject,cs_open_p,opened_by,cs_time_opened,
-         user_open_p,user_time_opened,privacy_level,trashed_p,
-         ignore_reopen_p,unscheduled_service_req_p,scheduled_operation_p,
-         scheduled_maint_req_p,priority)
-        values (:ticket_id,:instance_id,:contact_id,:authenticated_by,:ticket_category_id,
-                :current_tier_level,:subject,:cs_open_p,:opened_by,:cs_time_opened,
-                :user_open_p,:user_time_opened,:privacy_level,:trashed_p,
-                :ignore_reopen_p,:unscheduled_service_req_p,:scheduled_operation_p,
-                :scheduled_maint_req_p,:priority)
-    }
-
-    set message_ref [cs_ticket_message_create ticket_id $ticket_id contact_id $contact_id privacy_level $privacy_level subject $subject message $message internal_notes $internal_notes internal_p $internal_p]
-    if { $message_ref eq "" } {
-        set sucess_p 0
-    }
-    if { $success_p && $scheduled_maint_req_p && $scheduled_operations_p && $ann_type ne "" } {
-
-        # Operation has been scheduled with ticket creation.
-        # set any annoucements (and advanced notices ie scheduled messages) associated with scheduled event
-        if { $ann_message eq "" } {
-            set ann_message $message
+    set cs_reps_list [cs_support_reps_of_cat category_id $ticket_category_id]
+    if { [llength $cs_reps_list > 0 ]} {
+        set cc_reps_list [cs_contact_reps_of_cat contact_id $contact_id category_id $ticket_category_id]
+        if { [llength $cc_reps_list > 0 || ] } {
+            set ticket_id [cs_id_seq_nextval ticket_ref]
+            #set ticket_ref  --corresponds to ticket_id
+            ns_log Notice "cs_ticket_create ticket_id '${ticket_id}' by user_id '${user_id}'"
+            
+            if { $ann_type ne "" && [hf_list_filter_by_alphanum [list $ann_type]] } {
+                set ann_type [string range $ann_type 0 7]
+            } else {
+                set ann_type ""
+                ns_log Notice "cs_ticket_create: ann_type '${ann_type}' not valid. ignoring."
+            }
+            db_transaction {
+                db_dml cs_tickets_cr {insert into cs_tickets
+                    (ticket_id,instance_id,contact_id,authenticated_by,ticket_category_id,
+                     current_tier_level,subject,cs_open_p,opened_by,cs_time_opened,
+                     user_open_p,user_time_opened,privacy_level,trashed_p,
+                     ignore_reopen_p,unscheduled_service_req_p,scheduled_operation_p,
+                     scheduled_maint_req_p,priority)
+                    values (:ticket_id,:instance_id,:contact_id,:authenticated_by,:ticket_category_id,
+                            :current_tier_level,:subject,:cs_open_p,:opened_by,:cs_time_opened,
+                            :user_open_p,:user_time_opened,:privacy_level,:trashed_p,
+                            :ignore_reopen_p,:unscheduled_service_req_p,:scheduled_operation_p,
+                        :scheduled_maint_req_p,:priority)
+                }
+                cs_ticket_subscribe_contact_rep $ticket_id $cc_reps_list
+                cs_ticket_subscribe_support_rep $ticket_id $cs_reps_list
+            }
+            
+            set message_ref [cs_ticket_message_create ticket_id $ticket_id contact_id $contact_id privacy_level $privacy_level subject $subject message $message internal_notes $internal_notes internal_p $internal_p]
+            if { $message_ref eq "" } {
+                set sucess_p 0
+            }
+            if { $success_p && $scheduled_maint_req_p && $scheduled_operations_p && $ann_type ne "" } {
+                
+                # Operation has been scheduled with ticket creation.
+                # set any annoucements (and advanced notices ie scheduled messages) associated with scheduled event
+                if { $ann_message eq "" } {
+                    set ann_message $message
+                }
+                set success_p [cs_announcement_create $ann_message $ann_message_type $contact_id "" $begins $expiration $ticket_id $allow_html_p]
+                
+            }
+        } else {
+            ns_log Warning "cs_ticket_create.120: No contact reps found for ticket_category_id '${ticket_category_id}' instance_id '${instance_id}'"
         }
-        set success_p [cs_announcement_create $ann_message $ann_message_type $contact_id "" $begins $expiration $ticket_id $allow_html_p]
-
+    } else {
+        ns_log Warning "cs_ticket_create.123: No support reps found for ticket_category_id '${ticket_category_id}' instance_id '${instance_id}'"
     }
     if { $success_p } {
         set return_id $ticket_id
@@ -871,8 +883,6 @@ ad_proc -private cs_notify_contact_reps {
         set ticket_ref [cs_t_ref_from_id $ticket_id]
         set subject "#contact-support.ticket# #contact-support.Asset_Monitor# id ${monitor_id} for ${label}: ${alert_title}"
         set body $alert_message
-        # post to logged in user pages 
-        hf_log_create $asset_id "#contact-support.Asset_Monitor#" "alert" "id ${monitor_id} ${subject} \n Message: ${body}" $user_id $instance_id 
 
         # send email message
         append body "#contact-support.Alerts_can_be_customized#. #contact-support.See_asset_configuration_for_details#."
@@ -921,8 +931,6 @@ ad_proc -private cs_notify_support_reps {
         set ticket_ref [cs_t_ref_from_id $ticket_id]
         set subject "#contact-support.ticket# #contact-support.Asset_Monitor# id ${monitor_id} for ${label}: ${alert_title}"
         set body $alert_message
-        # post to logged in user pages 
-        hf_log_create $asset_id "#contact-support.Asset_Monitor#" "alert" "id ${monitor_id} ${subject} \n Message: ${body}" $user_id $instance_id 
 
         # send email message
         append body "#contact-support.Alerts_can_be_customized#. #contact-support.See_asset_configuration_for_details#."
